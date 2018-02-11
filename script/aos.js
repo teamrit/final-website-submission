b76cfb6d123e39c976fe0f56c29e5fcc8d895ce7
Amrit Pal Singh <teamrit@users.noreply.github.com>
2018-02-10 20:11:14 -0500

    Same commit from previous Assignment

---
+++ script/aos.js
@@ -0,0 +1,18 @@
+// opens the tab according to the button clicked
+function showTab(evt, cityName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+	document.getElementById(cityName).style.display = "block";
+
+	evt.currentTarget.className += " active";
+}
